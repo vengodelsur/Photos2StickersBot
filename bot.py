9ce787dec52044420bf94123323c1f72067aa65e
vengodelsur <ekatlmsh@yandex.ru>
2019-09-08 17:34:00 +0300

    delete unused imports

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 # encoding: utf-8
 import telebot
 import requests
-import urllib
-
+import skimage
 access_token = '615690891:AAGoQ8B1Q-4saF0dhyV6vJ8Syku3whOwBrQ'
 # Создание бота с указанным токеном доступа
 bot = telebot.TeleBot(access_token)
@@ -16,28 +15,38 @@ from PIL import Image
 
 @bot.message_handler(content_types=['photo'])
 def photo(message):
+    
     file_id = message.photo[-1].file_id
-    bot.send_photo(message.chat.id, file_id)
+    #bot.send_photo(message.chat.id, file_id)
     photo_file = bot.get_file(file_id)
     photo_file_path = photo_file.file_path
     url = 'https://api.telegram.org/file/bot' + access_token + '/' + photo_file_path
-    r = requests.get(url, allow_redirects=True, proxies=urllib.getproxies())
+    r = requests.get(url, allow_redirects=True)
+    print('connected')
     open('img.jpg', 'wb').write(r.content)
+    
     image = skimage.io.imread('img.jpg')
     result = neural_net.predict_by_image(image)
-    pil_image = apply_mask(image, result)
-    pil_image.save('img.jpg')
-    process_image('img.jpg') 
-    with open(im_fname, 'rb') as im_f:
-        bot.send_photo(message.chat.id, 'img.jpg')   
+    pil_image = neural_net.apply_mask(image, result)
+    
+    pil_image.save('img.webp')
+    object_number = 0
+    y1, x1, y2, x2 = result['rois'][object_number]
+    
+    process_image('img.webp', (x1 + x2)/2, (y1 + y2)/2) 
+     
+    with open('img.webp', 'rb') as f:
+        bot.send_document(message.chat.id, f)  
 
-def process_image(image_file_name):
+def process_image(image_file_name, w_center, h_center):
     img = Image.open(image_file_name)
     w, h = img.size
-    area = (w/2 - 512/2, h/2 - 512/2, w/2 + 512/2, h/2 + 512/2)
+    area = (w_center - 512/2, h_center - 512/2, w_center + 512/2, h_center + 512/2)
     cropped_img = img.crop(area)
     cropped_img.save(image_file_name)
 
 if __name__ == '__main__':
+    #photo('')
     bot.polling(none_stop=True)
+
 
--- neural_net.py
+++ neural_net.py
@@ -1,38 +1,54 @@
 import os
 import sys
-import random
-import math
 import numpy as np
-import skimage.io
-import matplotlib
-import matplotlib.pyplot as plt
+
 from PIL import Image
 
 # Root directory of the project
-ROOT_DIR = '/home/ekaterina/code/technosphere/project'
+ROOT_DIR = '.'
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn import utils
 import mrcnn.model as modellib
-from mrcnn import visualize
+# from mrcnn import visualize
 # Import COCO config
 # sys.path.append(os.path.join(ROOT_DIR, "/coco/"))  # To find local version
-import coco
+
+from mrcnn.config import Config
+
+class CocoConfig(Config):
+    """Configuration for training on MS COCO.
+    Derives from the base Config class and overrides values specific
+    to the COCO dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "coco"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 80  # COCO has 80 classes
+
 
 # Directory to save logs and trained model
-MODEL_DIR = '/home/ekaterina/code/technosphere/project/logs'
+MODEL_DIR = ROOT_DIR + '/logs'
 
 # Local path to trained weights file
-COCO_MODEL_PATH = '/home/ekaterina/code/technosphere/project/mask_rcnn_coco.h5'
+COCO_MODEL_PATH = ROOT_DIR + '/mask_rcnn_coco.h5'
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
 
 # Directory of images to run detection on
-IMAGE_DIR = '/home/ekaterina/code/technosphere/project/images'
+IMAGE_DIR = ROOT_DIR + '/images'
 
-class InferenceConfig(coco.CocoConfig):
+class InferenceConfig(CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
@@ -47,6 +63,7 @@ model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
 # Load weights trained on MS-COCO
 model.load_weights(COCO_MODEL_PATH, by_name=True)
+model.keras_model._make_predict_function()
 
 
 # COCO Class names
@@ -71,13 +88,14 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
 def predict_by_image(image):
     # image_path = '/home/ekaterina/code/technosphere/project/druid.jpg'
     # image = skimage.io.imread(image_path)
+    global model
     results = model.detect([image], verbose=1)
     return results[0]
 
 def apply_mask(image, result):
     object_number = 0
-    mask = np.where(result['masks'][:, :, object_number] == 0)
-    image[mask] = 0
-    return Image.fromarray(image, 'RGB')
+    # mask = np.where(result['masks'][:, :, object_number] == 0)
+    image = np.dstack([image,  (result['masks'][:, :, object_number]*255).astype(np.uint8)])
+    return Image.fromarray(image, 'RGBA')
 
 
